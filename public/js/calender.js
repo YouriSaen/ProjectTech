4f330bb4a7118ba0da6f9e0f19b8fedf0825dc8a
YouriSaen <youri.langenberg@hotmail.com>
2023-03-06 14:23:47 +0100

    merged css, changed profile, calender js added

---
+++ public/js/calender.js
@@ -0,0 +1,28 @@
+const morningSpace = document.querySelector(".morningSpace")
+const middaySpace = document.querySelector(".middaySpace");
+const eveningSpace = document.querySelector(".eveningSpace");
+
+
+function createSpaceMorning(){
+    const morningSpaceActivities = document.createElement('div');
+    morningSpaceActivities.className = 'activitiesSpaceMorning';
+    morningSpace.appendChild(morningSpaceActivities);
+}
+
+function createSpaceMidday(){
+    const middaySpaceActivities = document.createElement('div');
+    middaySpaceActivities.className = 'activitiesSpaceMidday';
+    middaySpace.appendChild(middaySpaceActivities);
+}
+
+function createSpaceEvening(){
+    const eveningSpaceActivities = document.createElement('div');
+    eveningSpaceActivities.className = 'activitiesSpaceEvening';
+    eveningSpace.appendChild(eveningSpaceActivities);
+}
+
+for(i = 0; i < 7; i++){
+    createSpaceMorning();
+    createSpaceMidday();
+    createSpaceEvening();
+} 
