9a7fd03be14577543ffd303027725f4df8db2cea
YouriSaen <youri.langenberg@hotmail.com>
2023-03-10 16:09:01 +0100

    Made second screen and set up server with data

--- server.js
+++ server.js
@@ -1,34 +1,66 @@
-
+require('dotenv').config();
 //Loads Express module
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 //Loads the handlebars module
 const handlebars = require('express-handlebars');
+const { MongoClient, ServerApiVersion} = require('mongodb');
+
+const uri =
+    "mongodb+srv://" +
+    process.env.DB_USERNAME +
+    ":" +
+    process.env.DB_PASS +
+    "@" +
+    process.env.DB_NAME +
+    "." +
+    process.env.DB_HOST +
+    "/" +
+    "?tls=true&retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+
+// client.connect()
+//     .then((res) => console.log('@@-- connection established', res))
+//     .catch((err) => console.log('@@-- error', err));
+
+
+client.connect()
+    .then((res) => console.log('@@-- connection established', res))
+    .catch((err) => console.log('@@-- error', err));
+
+
+async function getData(){
+    const collection = await client.db('calenderdb').collection('activities');
+    const docs = await collection.find().toArray();
+    console.log(docs);
+}
+
+
 
 //Sets our app to use the handlebars engine
 app.set('view engine', 'hbs');
-
-
 app.use(express.static('public'));
-
 //Sets handlebars configurations (we will go through them later on)
 app.engine('hbs', handlebars.engine({
-layoutsDir: __dirname + '/views/layouts',
-extname: 'hbs',
-defaultLayout: 'index',
-partialsDir: __dirname + '/views/partials/'
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'index',
+    partialsDir: __dirname + '/views/partials/'
 }));
 
 
 app.get('/', (req, res) => {
-res.render('main', {layout: 'index'});
+    res.render('main', { layout: 'index' });
 });
 
-app.get('/activities', (req, res) => {
-    res.render('activities', {layout: 'activities'});
-    });
-
+app.get('/activities', async (req, res) => {
+    res.render('activities', { layout: 'activities' });
+    getData();
+    
+});
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
