abeb0a84dfb0e40029ac8fe362bb5cd8603c5f85
YouriSaen <youri.langenberg@hotmail.com>
2023-03-23 11:41:03 +0100

    Debugging and trying to figure out changing cllect

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 //Loads Express module
 const express = require('express');
+var bodyParser = require('body-parser')
+const http = require('http');
+const session = require('express-session');
 const app = express();
+const server = http.createServer(app); 
 
 const port = process.env.PORT || 3000;
 
@@ -12,6 +16,7 @@ const {
     MongoClient,
     ServerApiVersion
 } = require('mongodb');
+const { css } = require('js-beautify');
 
 const uri =
     "mongodb+srv://" +
@@ -45,9 +50,17 @@ client.connect()
 //Sets our app to use the handlebars engine
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
+app.use(express.json());   
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({
     extended: true
 }));
+app.use(session({
+    secret: 'my-secret',
+    resave: false,
+    saveUninitialized: true
+  }));
 //Sets handlebars configurations (we will go through them later on)
 app.engine('hbs', handlebars.engine({
     layoutsDir: __dirname + '/views/layouts',
@@ -64,6 +77,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/activities', async (req, res) => {
+    const dbName = req.body.timespaceBtn;
+    req.dbName = dbName;
+
     const activitiesCollection = await client.db('calenderdb').collection('activities');
     // const activities = await activitiesCollection.find().toArray();
     const query = {};
@@ -71,44 +87,62 @@ app.get('/activities', async (req, res) => {
         name: 1,
         _id: 0
     };
-    const cursor = activitiesCollection.find(query, projection);
-    const data = await cursor.toArray();
-    const namesNmb = data.length;
-    const names = data.map(data => data.name);
+    const cursor = await activitiesCollection.find(query, projection).toArray();
+    // const data = await cursor.toArray();
+    console.log(cursor);
+    const namesNmb = cursor.length;
+    const names = cursor.map(data => data.name);
     // console.log(names);
-    let a = {};
+    let a = [];
+    cursor.map((elem) => a.push(elem.name));
 
-    for (var i = 0; i < data.length; i++) {
-        a = names[i]
-    }
-    // console.log(a);
+    const activatedCollectionMorning = await client.db('calenderdb').collection('morning');
+    const activatedCursorMorning = activatedCollectionMorning.find(query, projection);
+    const activatedDataMorning = await activatedCursorMorning.toArray();
+    const activatedNamesMorning = activatedDataMorning.map(activatedData => activatedData.name);
+    const activatedNmbMorning = activatedNamesMorning.length;
 
+    const activatedCollectionMidday  = await client.db('calenderdb').collection('midday');
+    const activatedCursorMidday = activatedCollectionMidday.find(query, projection);
+    const activatedDataMidday = await activatedCursorMidday.toArray();
+    const activatedNamesMidday = activatedDataMidday.map(activatedData => activatedData.name);
+    const activatedNmbMidday = activatedNamesMidday.length;
 
-
-    const activatedCollection = await client.db('calenderdb').collection('morning');
-    const activatedCursor = activatedCollection.find(query, projection);
-    const activatedData = await activatedCursor.toArray();
-    const activatedNames = activatedData.map(activatedData => activatedData.name);
-    const activatedNmb = activatedNames.length;
+    const activatedCollectionEvening = await client.db('calenderdb').collection('evening');
+    const activatedCursorEvening = activatedCollectionEvening.find(query, projection);
+    const activatedDataEvening = await activatedCursorEvening.toArray();
+    const activatedNamesEvening = activatedDataEvening.map(activatedData => activatedData.name);
+    const activatedNmbEvening = activatedNamesEvening.length;
 
     // console.log(activities);
     res.render('activities', {
         layout: 'activities',
-        activatedActivity: activatedNames,
-        activatedNmb: activatedNmb,
+        activatedActivityMorning: activatedNamesMorning,
+        activatedActivityMidday: activatedNamesMidday,
+        activatedActivityEvening: activatedNamesEvening,
+        activatedNmb: activatedNmbMorning,
         activity: a,
         namesLength: namesNmb
     })
-
 });
 
+const ObjectId = require('mongodb').ObjectId;
+
+app.post('/post-db', (req, res) => {
+    const dbName = req.body.timespaceBtn;
+    req.dbName = dbName;
+    res.redirect('/activities');
+});
+
+
 app.post('/post-name', (req, res) => {
-    // let collectionTimespace = JSON.stringify(req.body.timespaceBtn);
-    // console.log(collectionTimespace);
-    const activitiesCollection = client.db('calenderdb').collection('morning');
-    const name = req.body.name;
-    console.log('Name:', name);
-    activitiesCollection.insertOne({
+    const name = req.body.activity;
+    const dbName = req.dbName;
+    console.log("READ THIS:::String", dbName);
+    let activatedCollection = client.db('calenderdb').collection('morning');
+
+    activatedCollection.insertOne({
+        _id: new ObjectId(), // generate a new unique identifier for each document
         name
     }, (err, result) => {
         if (err) {
@@ -123,20 +157,25 @@ app.post('/post-name', (req, res) => {
             message: 'Name inserted successfully'
         });
     });
+
     res.redirect('/activities');
 });
 
 app.get('/deleteData', function(req, res) {
     const deleteName = req.body.deleteName;
-    const activatedCollection = client.db('calenderdb').collection('morning');
-    console.log('Deleted data with ID ' + deleteName);
+    const activatedCollection = client.db('calenderdb').collection('midday');
+    // console.log('Deleted data with ID ' + deleteName);
     activatedCollection.deleteOne({
         deleteName
     }, function(err, result) {
         if (err) throw err;
-        console.log('Deleted data with ID ' + deleteName);
+        // console.log('Deleted data with ID ' + deleteName);
 
     });
     res.redirect('/activities');
 });
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+  })
 app.listen(port, () => console.log(`App listening to port ${port}`));
